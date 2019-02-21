93af4dece29f5202d1057fce511d54a1b8c3b618
NelsonKioko <kiokonelson2@gmail.com>
2019-02-21 09:10:54 +0300

    make the fun game work

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+        var person1Input = $("input#person1").val();
+        var person2Input = $("input#person2").val();
+        var animalInput = $("input#animal").val();
+        var exclamationInput = $("input#exclamation").val();
+        var verbInput = $("input#verb").val();
+        var nounInput = $("input#noun").val();
+        $(".person1").text(person1Input);
+        $(".person2").text(person2Input);
+        $(".animal").text(animalInput);
+        $(".exclamation").text(exclamationInput);
+        $(".verb").text(verbInput);
+        $(".noun").text(nounInput);
+
+        $("#story").show();
+
+        event.preventDefault();
+    });
+});
+ /*this the same code without repetition
+ $(document).ready(function() {
+    $("#blanks form").submit(function(event) {
+      var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+      blanks.forEach(function(blank) {
+        var userInput = $("input#" + blank).val();
+        $("." + blank).text(userInput);
+      });
+
+      $("#story").show();
+
+      event.preventDefault();
+    });
+  });*/
